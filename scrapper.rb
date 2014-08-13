28c0bbb3cffd2c0a5e07f82b01e86f0bc16f2e3a
indotek68 <angga.w.putra@gmail.com>
2014-08-12 23:28:56 -0700

    function and testing passed

--- scrapper.rb
+++ scrapper.rb
@@ -1,36 +1,49 @@
 # scrapper.rb
 require 'nokogiri'
 require 'open-uri'
+require 'awesome_print'
+
+# url = "today.html"
+# page = Nokogiri::HTML(open(url))
+# date = page.css(".row span .date").text
+# text = page.css(".row a").text
 
 
-
-def filter_links(rows, regex)
-  # takes in rows and returns uses
-  # regex to only return links 
-  # that have "pup", "puppy", or "dog"
-  # keywords
+def get_todays_rows(doc, date_str, regex)
+  today_results = doc.css(".row").select do |link|
+    link.css(".date").text == date_str &&
+    link.css(".hdrlnk").text.match(regex) &&
+    !link.css(".p").text.nil?
+    # regex
+  end
+  # ap today_results
 end
 
-def get_todays_rows(doc, date_str)
-  #  1.) open chrome console to look in inside p.row to see
-  #  if there is some internal date related content
-
-  #  2.) figure out the class that you'll need to select the
-  #   date from a row
-
-end
-
-def get_page_results
-  url = "http://sfbay.craigslist.org/sfc/pet/"
-  Nokogiri::HTML(open(url))
+def get_page_results(date_str)
+  dog_regex = /puppy|pup|dog/
+  url = "today.html"
+  page = Nokogiri::HTML(open(url))
+  get_todays_rows(page, date_str, dog_regex)
 end
 
 def search(date_str)
-  get_page_results
+  data = get_page_results(date_str)
+  resultsArray = []
+
+  data.each do |results|
+    title = results.css(".hdrlnk").text
+    link = results.css(".hdrlnk").first["href"]
+    
+    resultsArray.push({title: title, link: link})
+  end
+  ap resultsArray
 end
 
 # want to learn more about 
 # Time in ruby??
 #   http://www.ruby-doc.org/stdlib-1.9.3/libdoc/date/rdoc/Date.html#strftime-method
 today = Time.now.strftime("%b %d")
+# today = "Aug 12"
 search(today)
+
+
--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -8,25 +8,43 @@ describe "scrapper" do
     #   problematic if you were blocked
     @doc = Nokogiri::HTML(open("today.html"))
     @today = "Aug 12"
+    @dog_regex = /puppy|pup|dog/
   end
 
-  describe "filter_links" do
-    it "should here is an example of using the befor each data" do
-      rows = @doc.css(".row")
-      rows.each do |row|
-        puts row.content
-      end
-      expect("your attention").to eql("this test to be erased or edited")
-    end
+  # describe "filter_links" do
+  #   it "should here is an example of using the befor each data" do
+  #     rows = @doc.css(".row")
+  #     rows.each do |row|
+  #       puts row.content
+  #     end
+  #     expect("your attention").to eql("this test to be erased or edited")
+  #   end
 
-    it "you can also use a string" do
-      # rowsString is contains one valid row
-      rowsString =  '<p class="row" data-pid="4581207234"> <a href="/sfc/pet/4581207234.html" class="i" data-id="0:00F0F_kUje2CUzekG"></a> <span class="txt"> <span class="star"></span> <span class="pl"> <span class="date">Aug 12</span>  <a href="/sfc/pet/4581207234.html" data-id="4581207234" class="hdrlnk">$25/night In Home Dog Sitter! Small Dog Retreat &#9829;</a> </span> <span class="l2">   <span class="pnr"> <small> (Bay Area)</small> <span class="px"> <span class="p"> pic&nbsp;<span class="maptag" data-pid="4581207234">map</span></span></span> </span>  </span> </span> </p>'
-      rows = Nokogiri::HTML(rowsString)
-      expect(filter_links(rows, @today).length).to eql(1)
-    end
-  end
+  #   it "you can also use a string" do
+  #     # rowsString is contains one valid row
+  #     rowsString =  '<p class="row" data-pid="4581207234"> <a href="/sfc/pet/4581207234.html" class="i" data-id="0:00F0F_kUje2CUzekG"></a> <span class="txt"> <span class="star"></span> <span class="pl"> <span class="date">Aug 12</span>  <a href="/sfc/pet/4581207234.html" data-id="4581207234" class="hdrlnk">$25/night In Home Dog Sitter! Small Dog Retreat &#9829;</a> </span> <span class="l2">   <span class="pnr"> <small> (Bay Area)</small> <span class="px"> <span class="p"> pic&nbsp;<span class="maptag" data-pid="4581207234">map</span></span></span> </span>  </span> </span> </p>'
+  #     rows = Nokogiri::HTML(rowsString)
+  #     expect(filter_links(rows, @today).length).to eql(1)
+  #   end
+  # end
 
   describe "get_todays_rows" do
+    it "should result to 8 items in the array" do
+      rows = @doc.css(".row")
+      expect(get_todays_rows(rows, @today, @dog_regex).length).to eql(9)
+    end
   end
+
+  describe "get_page_results" do
+    it "should result to 8 items in the array" do
+      expect(get_page_results(@today).length).to eql(9)
+    end
+  end
+
+  describe "search" do
+    it "should result to 8 items in the array" do
+      expect(search(@today).length).to eql(9)
+    end
+  end
+
 end
